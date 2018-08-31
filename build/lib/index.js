198651af31a0f5b96e016f2b16f7ae9ff236c8dc
Anton <anton@adc.sh>
2018-09-01 02:58:33 +0300

    build

---
+++ build/lib/index.js
@@ -0,0 +1,6 @@
+       const commentsRe =  /\/\*(?:[\s\S]+?)\*\//g
+       const inlineCommentsRe = /\/\/(.+)/gm
+
+module.exports.commentsRe = commentsRe
+module.exports.inlineCommentsRe = inlineCommentsRe
+//# sourceMappingURL=index.js.map
