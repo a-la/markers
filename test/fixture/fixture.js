4b5ff49e430c3cf5649b3cadb0696aa475259afc
Anton <anton@adc.sh>
2018-09-01 06:55:52 +0300

    update order

--- test/fixture/fixture.js
+++ test/fixture/fixture.js
@@ -1,6 +1,10 @@
+import test from '../../src/lib'
+
 const re = /test\//
 const re2 = /test2[t/]/
 const re3 = /test3\[]/
+const re4 = /test`/
+const re5 = /test`/
 
 const s = 'string\''
 const s2 = `string\``
--- test/spec/escapes.js
+++ test/spec/escapes.js
@@ -1,13 +1,14 @@
 import { equal } from 'zoroaster/assert'
 import { Replaceable } from 'restream'
 import Catchment from 'catchment'
+import { createReadStream } from 'fs'
+import SnapshotContext from 'snapshot-context'
 import Context from '../context'
 import makeRules from '../../src'
-import { createReadStream } from 'fs'
 
-/** @type {Object.<string, (c: Context)>} */
+/** @type {Object.<string, (c: Context, sc: SnapshotContext)>} */
 const T = {
-  context: Context,
+  context: [Context, SnapshotContext],
   async 'pastes markers back'({ JS_FIXTURE, readFile }) {
     const { rules } = makeRules()
     const rs = new Replaceable(rules)
@@ -17,6 +18,25 @@ const T = {
     const res = await promise
     const expected = await readFile(JS_FIXTURE)
     equal(res, expected)
+  },
+  async 'cuts strings before regexes'(
+    { JS_FIXTURE, SNAPSHOT_DIR }, { setDir, test },
+  ) {
+    setDir(SNAPSHOT_DIR)
+    let m
+    const { rules } = makeRules([{
+      re: /[\s\S]+/g,
+      replacement(match) {
+        m = match
+        return match
+      },
+    }])
+    const rs = new Replaceable(rules)
+    const { promise } = new Catchment({ rs })
+    const readable = createReadStream(JS_FIXTURE)
+    readable.pipe(rs)
+    await promise
+    await test('order.js', m)
   },
 }
 
